c07f53994851d5384211622d5d28e8a802360708
BryGojo <34994@ocvts.org>
2025-01-10 13:49:31 +0000

    Image Gallery Update 2

--- script.js
+++ script.js
@@ -1,14 +1,50 @@
+// Select all images in the gallery
 const galleryImages = document.querySelectorAll('#gallery-container img');
+const images = Array.from(galleryImages); // Convert nodeList to array
+// Select elements for lightbox
 const lightbox = document.getElementById('lightbox');
-const lightboxImg = document.getElementById('lightbox-image');
+const lightboxImg = document.getElementById('lightbox-img');
 
-galleryImages.forEach(Image => {
-    Image.addEventListener('click', () => {
+let currentIndex = 0; //Track the currently displayed image
+
+// Open lightbox and synchronize currentIndex when an an image is clicked
+images.forEach(image, index => {
+    image.addEventListener('click', () => {
+        currentIndex = index;
         lightboxImg.src = image.src;
         lightbox.classList.add('visible');
     });
 });
 
-lightbox.addEventListener('click', () => {
+
+// Close lightbox when clicking outside the content 
+lightbox.addEventListener('click', (event) => {
+    if (event.target === lightbox) {
     lightbox.classList.remove('visible');
+    }
+});
+
+//Show next inage in the lightbox
+document.getElementById('next').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1 ) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+});
+
+// Show previous image in the lightbox
+document.getElementById('prev').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    lightboxImg.src = images[currentIndex].src;
+});
+
+// Keyboard navigation for the lightbox
+document.addEventListener('keydown',(event) => {
+    if (event.key === 'ArrowRight') {
+        currentIndex = (currentIndex + 1) % images.length;
+        lightboxImg.src = images[currentIndex].src;
+    } else if (event.key === 'Arrowleft') {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        lightboxImg.src = images[currentIndex].src;
+    } else if (event.key === 'Escape') {
+        lightbox.classLiss.remove('visible');
+    }
 })
